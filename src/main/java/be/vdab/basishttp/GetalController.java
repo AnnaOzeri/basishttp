c344c55e09005ebbfe588eac3a8c63fea71bc384
Anna Ozeriannaia <aeo040191@gmail.com>
2024-02-19 13:55:26 +0100

    @RestController

--- src/main/java/be/vdab/basishttp/GetalController.java
+++ src/main/java/be/vdab/basishttp/GetalController.java
@@ -1,7 +1,19 @@
 package be.vdab.basishttp;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class GetalController {
+
+    @GetMapping("geluk")
+    int geluksGetal() {
+        return 7;
+    }
+
+    @GetMapping("ongeluk")
+    int ongeluksGetal() {
+        return 666;
+    }
+
 }
